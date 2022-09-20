1964671718537219e97122d84817f2de7497d29a
santosh <santosh33@gmail.com>
2022-09-20 18:38:38 +0530

    make changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import {json} from  'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser',(req,res)=>{
+    res.send("Hi there");
+});
+
 app.listen(3000, ()=>{
     console.log("express application - Listening on port 3000!")
 });
