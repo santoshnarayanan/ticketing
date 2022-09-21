c77b3f53b916032970b406eabeda2609f45d825c
santosh <santosh33@gmail.com>
2022-09-21 22:51:15 +0530

    Formatting

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
-import { DatabaseConnectionError } from './../errors/database-connection-error';
 import express, {Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
-import { RequestValidationError, DatabaseConnectionError } from '../errors/request-validation-error';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from './../errors/database-connection-error';
 
 const router = express.Router();
 
