f8bd6a97107c846b97c14d43921b2f284c7f199a
santosh <santosh33@gmail.com>
2022-09-22 00:09:41 +0530

    Verifying CustomError and Final Error Code Picture

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
-import { RequestValidationError } from '../errors/request-validation-error';
+import { CustomError } from '../errors/custom-error';
 
 
 export const errorHandler = (
@@ -9,12 +8,8 @@ export const errorHandler = (
     res: Response, 
     next: NextFunction
 )=> {
-    if(err instanceof RequestValidationError) {
+    if(err instanceof CustomError) {
         return res.status(err.statusCode).send({errors:err.serializeErrors() });
-    }
-
-    if(err instanceof DatabaseConnectionError) {
-        return res.status(err.statusCode).send({errors: err.serializeErrors()});
     }
 
     res.status(400).send({
