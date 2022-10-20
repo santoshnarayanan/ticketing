56729e97b6abc7fdae9c15f698f6119468361b17
Santosh Narayanan <santosh33@gmail.com>
2022-10-20 15:50:49 +0200

    Mongoose Pre-Save Hooks, test new hashed pwd

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,38 +1,51 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 //! An interface that describes proprties that are required
 //! to create a new user
-interface UserAttrs{
-    email: string;
-    password:string;
+interface UserAttrs {
+  email: string;
+  password: string;
 }
 
-//! An interface describes the properties that 
+//! An interface describes the properties that
 //! user model has
-interface UserModel extends mongoose.Model<UserDoc>{
-    build(attrs:UserAttrs): UserDoc;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
-//! An interface describes the properties that 
+//! An interface describes the properties that
 //! user model has
-interface UserDoc extends mongoose.Document{
-    email: string;
-    password:string
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
 }
 
-const userSchema  = new mongoose.Schema({
-    email:{
-        type:String, required: true
-    },
-    password:{
-        type:String, required: true
-    }
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-userSchema.statics.build = (attrs:UserAttrs) => {
-    return new User(attrs)
-}
+//! implemented middleware function in mongoose
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+      const hashed = await Password.toHash(this.get('password'));
+      this.set('password', hashed);
+    }
+    done();
+  });
 
-const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
-export  {User};
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
