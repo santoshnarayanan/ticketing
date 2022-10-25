48e5cce6df29ee138fc03d8b9fdadba49090dd2c
Santosh Narayanan <santosh33@gmail.com>
2022-10-25 19:45:11 +0200

    Index.ts  is factored to app.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from  'express';
-import 'express-async-errors';
-import {json} from  'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true); // traffic is routed through ingrex-ngnix
-app.use(json());
-app.use(cookieSession({
-    signed:false, //encryption because jwt is encryted
-    secure:true   // https
-})
-);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-// ! below checks for non route exists
-app.all('*',async (req, res)=>{
-    throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app';
 
 // ! connect to mongodb instance
 const start = async ()=>{
