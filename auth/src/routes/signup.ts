70492c80ce1a6151ae1f7cce14839df5cb78e385
Santosh Narayanan <santosh33@gmail.com>
2022-10-25 12:11:38 +0200

    Common Request Validation Middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,51 +1,50 @@
-import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from './../errors/database-connection-error';
-import { setOriginalNode } from 'typescript';
-import { BadRequestError } from './../errors/bad-request-error';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "./../errors/bad-request-error";
 
 const router = express.Router();
 
-router.post('/api/users/signup',[
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').isLength({min:4, max:20})
-    .withMessage('Password must be between 4 and 20 characters')
-],
-async (req :Request, res: Response)=>{
-    const errors = validationResult(req);
-    
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
     }
 
-    const {email,password} = req.body;
-
-    const existingUser = await User.findOne({email});
-
-    if(existingUser){
-        throw new BadRequestError('Email in use');
-    }
-
-    const user = User.build({email, password});
+    const user = User.build({ email, password });
     await user.save(); //saving details to mongoDb
 
     // Generate JWT
-    const userJwt = jwt.sign({
-        id:user.id,
-        email:user.email
-    }, 
-    process.env.JWT_KEY!
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
     );
 
     //Store in session object
     req.session = {
-        jwt:userJwt
+      jwt: userJwt,
     };
 
-    res.status(201).send(user); //record is created 
-});
+    res.status(201).send(user); //record is created
+  }
+);
 
-export {router as signUpRouter};
+export { router as signUpRouter };
