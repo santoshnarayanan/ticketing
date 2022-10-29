dd9666c6f2adebb0a00b7c98e86c934352a3c66f
Santosh Narayanan <santosh33@gmail.com>
2022-10-29 23:37:48 +0200

    Handling errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,10 +4,17 @@ import axios from "axios";
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
     const onSubmit = async (event)=>{
         event.preventDefault();
-        const response = await axios.post('/api/users/signup',{email,password});
-        console.log(response.data);
+        try {
+          const response = await axios.post('/api/users/signup',{email,password});
+          console.log(response.data);
+        } catch (err) {
+          setErrors(err.response.data.errors);
+        }
+
     };
   return (
     <form onSubmit={onSubmit}>
@@ -21,6 +28,13 @@ export default () => {
         <input value={password} onChange={e=>setPassword(e.target.value)} type="password"
                className="form-control"/>
       </div>
+        {errors.length > 0  && <div className="alert alert-danger">
+          <h4>Oops.....</h4>
+          <ul className="my-0">
+            {errors.map(err=><li key={err.message}>{err.message}</li>)}
+          </ul>
+        </div>}
+
       <button className="btn btn-primary">
       Sign Up
       </button>
