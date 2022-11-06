bffd01a6e883b38440d67a8cee2d575d4e4c1165
Santosh Narayanan <santosh33@gmail.com>
2022-11-06 18:13:04 +0100

    Specify Host

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,14 +9,24 @@ const LandingPage = ({currentUser})=>{
 //always on server side rendering
  LandingPage.getInitialProps = async () =>{
     //const response = await axios.get('/api/users/currentuser');
-
     //return response.data;
      if(typeof window === 'undefined'){
          //we are on the server
-         //requests should be made to http://ingressnamespace.service....
+         //requests should be made to http://service.namespace....
+         const {data} = await axios.get(
+             'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+             {
+                 headers: {
+                     Host:'ticketing.local'
+                 }
+             }
+         );
+         return data;
      }
      else {
          //we are on the browser request can be made with base url of ''
+         const {data} = await axios.get('/api/users/currentuser');
+         return data;
      }
      //console.log("I was executed");
      return{};
