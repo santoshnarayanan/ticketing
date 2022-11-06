185e8f1814132a2328325d64397cba154d7e474f
Santosh Narayanan <santosh33@gmail.com>
2022-11-06 23:04:27 +0100

    Reusable API Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,14 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
-const LandingPage = ({currentUser})=>{
-    //console.log(currentUser);
-    //axios.get('/api/users/currentuser');
+const LandingPage = ()=>{
     return <h1>Landing Page</h1>;
 };
 
 //always on server side rendering
- LandingPage.getInitialProps = async ({req}) =>{
-     //console.log(req.headers);
-     if(typeof window === 'undefined'){
-         //we are on the server
-         //requests should be made to http://service.namespace....
-         const {data} = await axios.get(
-             'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-             {headers: req.headers }
-         );
-         return data;
-     }
-     else {
-         //we are on the browser request can be made with base url of ''
-         const {data} = await axios.get('/api/users/currentuser');
-         return data;
-     }
+ LandingPage.getInitialProps = async (context) =>{
+     const client = buildClient(context);
+     const {data} = await client.get('/api/users/currentuser');
+     return data;
 };
 
 export default  LandingPage;
